53f63b1bfbc1ec016f7c76169ddae2bac3122525
Jan Pochyla <jpochyla@gmail.com>
2020-10-13 11:43:56 +0200

    click: allow configuring the mouse button

--- druid/src/widget/click.rs
+++ druid/src/widget/click.rs
@@ -38,6 +38,8 @@ use crate::{Data, Env, Event, EventCtx, LifeCycle, LifeCycleCtx, MouseButton, Wi
 pub struct Click<T> {
     /// A closure that will be invoked when the child widget is clicked.
     action: Box<dyn Fn(&mut EventCtx, &mut T, &Env)>,
+    /// Mouse button this widget reacts to. Defaults to the left button.
+    button: MouseButton,
 }
 
 impl<T: Data> Click<T> {
@@ -45,7 +47,14 @@ impl<T: Data> Click<T> {
     pub fn new(action: impl Fn(&mut EventCtx, &mut T, &Env) + 'static) -> Self {
         Click {
             action: Box::new(action),
+            button: MouseButton::Left,
         }
+    }
+
+    /// Builder-style method for setting the mouse button.
+    pub fn with_button(mut self, button: MouseButton) -> Self {
+        self.button = button;
+        self
     }
 }
 
@@ -53,13 +62,13 @@ impl<T: Data, W: Widget<T>> Controller<T, W> for Click<T> {
     fn event(&mut self, child: &mut W, ctx: &mut EventCtx, event: &Event, data: &mut T, env: &Env) {
         match event {
             Event::MouseDown(mouse_event) => {
-                if mouse_event.button == MouseButton::Left {
+                if mouse_event.button == self.button {
                     ctx.set_active(true);
                     ctx.request_paint();
                 }
             }
             Event::MouseUp(mouse_event) => {
-                if ctx.is_active() && mouse_event.button == MouseButton::Left {
+                if ctx.is_active() && mouse_event.button == self.button {
                     ctx.set_active(false);
                     if ctx.is_hot() {
                         (self.action)(ctx, data, env);
